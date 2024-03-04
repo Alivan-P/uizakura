24489eebc68fa55167cca3a16b2bb4c457eae66a
wen <alwjlola@gmail.com>
2024-03-04 15:24:12 +0800

    add AsyncData list

--- lib/src/async_state/async_state.dart
+++ lib/src/async_state/async_state.dart
@@ -5,47 +5,59 @@ part 'async_state.freezed.dart';
 abstract class _AsyncData<T> {
   String get message;
 
-  /// 业务状态码
   String get code;
 
   T? get data;
+
+  List<T>? get listData;
 }
 
 @freezed
 class Async<T> with _$Async<T> {
   @Implements.fromString('_AsyncData<T>')
-  factory Async.initializing(
-      {@Default("") String message,
-      @Default("") String code,
-      T? data}) = Initializing;
+  factory Async.initializing({
+    @Default("") String message,
+    @Default("") String code,
+    T? data,
+    List<T>? listData,
+  }) = Initializing;
 
   @Implements.fromString('_AsyncData<T>')
   factory Async.refreshing(
       {@Default("") String message,
       @Default("") String code,
+      List<T>? listData,
       T? data}) = Refreshing;
 
   @Implements.fromString('_AsyncData<T>')
   factory Async.loading(
       {@Default("") String message,
       @Default("") String code,
+        List<T>? listData,
       T? data}) = Loading;
 
   @Implements.fromString('_AsyncData<T>')
   factory Async.success(
       {@Default("") String message,
       @Default("") String code,
+      List<T>? listData,
       T? data}) = Success;
 
   @Implements.fromString('_AsyncData<T>')
-  factory Async.error(
-      {@Default("") String message, @Default("") String code, T? data}) = Error;
+  factory Async.error({
+    @Default("") String message,
+    @Default("") String code,
+    T? data,
+    List<T>? listData,
+  }) = Error;
 
   @Implements.fromString('_AsyncData<T>')
-  factory Async.uninitialized(
-      {@Default("") String message,
-      @Default("") String code,
-      T? data}) = Uninitialized;
+  factory Async.uninitialized({
+    @Default("") String message,
+    @Default("") String code,
+    List<T>? listData,
+    T? data,
+  }) = Uninitialized;
 }
 
 extension AsyncExtension on Async<dynamic> {
