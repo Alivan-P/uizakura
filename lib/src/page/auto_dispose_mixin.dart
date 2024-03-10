9c67e45072a5b9ed031d7491c9153f3a9690a2fa
wen <alwjlola@gmail.com>
2024-03-10 22:27:37 +0800

    catch _disposeSet error

--- lib/src/page/auto_dispose_mixin.dart
+++ lib/src/page/auto_dispose_mixin.dart
@@ -1,35 +1,24 @@
 import 'package:flutter/cupertino.dart';
-import 'package:uizakura/uizakura.dart';
 
 /// @author luwenjie on 2024/2/28 23:04:09
 
 mixin AutoDisposeMixin<T extends StatefulWidget> on State<T> {
   final _disposeSet = <Function?>[];
-  final _disposeFutures = <Future<dynamic>>[];
 
   @protected
   void addDispose(Function block) async {
     _disposeSet.add(block);
   }
 
-  @protected
-  void addDisposeFuture(Future<dynamic> future) async {
-    _disposeFutures.add(future);
-  }
-
   @override
   @mustCallSuper
   void dispose() {
     for (var element in _disposeSet) {
-      element?.call();
-    }
-    // ignore disposed futures result
-    try {
-      for (Future<dynamic> element in _disposeFutures) {
-        element.ignore();
+      try {
+        element?.call();
+      } catch (e) {
+        debugPrint("AutoDisposeMixin error on $e");
       }
-    } catch (e) {
-      //
     }
     super.dispose();
   }
